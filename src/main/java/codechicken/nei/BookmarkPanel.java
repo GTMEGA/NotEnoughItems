153da523d608d580f18b863eea2b33dd7eaebb8e
Nico Spiske <64710705+Nilau1998@users.noreply.github.com>
2023-09-08 07:16:37 +0200

    Hide bookmarkItemPulling button if currently opened gui is not supported (#419)
    * [minor] hide pullBookmarkedItems button if open container is not supported
    
    * [minor] spotless apply

--- src/main/java/codechicken/nei/BookmarkPanel.java
+++ src/main/java/codechicken/nei/BookmarkPanel.java
@@ -868,7 +868,9 @@ public class BookmarkPanel extends PanelWidget {
             LayoutManager.addWidget(namespacePrev);
             LayoutManager.addWidget(namespaceNext);
             LayoutManager.addWidget(namespaceLabel);
-            LayoutManager.addWidget(pullBookmarkedItems);
+            if (BookmarkContainerInfo.getBookmarkContainerHandler(getGuiContainer()) != null) {
+                LayoutManager.addWidget(pullBookmarkedItems);
+            }
         }
     }
 
@@ -1056,7 +1058,7 @@ public class BookmarkPanel extends PanelWidget {
             tooltip.add(translate("bookmark.viewmode.toggle.tip"));
         }
         if (new Rectangle4i(pullBookmarkedItems.x, pullBookmarkedItems.y, pullBookmarkedItems.w, pullBookmarkedItems.h)
-                .contains(mx, my)) {
+                .contains(mx, my) && BookmarkContainerInfo.getBookmarkContainerHandler(getGuiContainer()) != null) {
             tooltip.add(translate("bookmark.pullBookmarkedItems.tip"));
         }
 
