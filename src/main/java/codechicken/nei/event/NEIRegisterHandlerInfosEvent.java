151a9957de68f97fc55c352f31d6ec78e8d3d272
miozune <miozune@gmail.com>
2023-11-07 22:44:04 +0900

    Add another method for handler info event (#438)

--- src/main/java/codechicken/nei/event/NEIRegisterHandlerInfosEvent.java
+++ src/main/java/codechicken/nei/event/NEIRegisterHandlerInfosEvent.java
@@ -12,25 +12,28 @@ import cpw.mods.fml.common.eventhandler.Event;
 
 /**
  * Event is posted every time the handler infos got registered or reloaded. During it, you can safely register your
- * custom handler info.
- *
- * <br>
- * This event does not have a result. {@link HasResult}<br>
+ * custom handler info. <br>
+ * This event does not have {@link HasResult result}.<br>
  * <br>
  * This event is fired on the {@link MinecraftForge#EVENT_BUS}.
  */
+@SuppressWarnings("unused")
 public class NEIRegisterHandlerInfosEvent extends Event {
+
+    public void registerHandlerInfo(HandlerInfo info) {
+        if (GuiRecipeTab.handlerMap.put(info.getHandlerName(), info) != null) {
+            NEIClientConfig.logger.info("Replaced handler info for {}", info.getHandlerName());
+        } else {
+            NEIClientConfig.logger.info("Added handler info for {}", info.getHandlerName());
+        }
+    }
 
     public void registerHandlerInfo(String handlerName, String modName, String modId,
             Consumer<HandlerInfo.Builder> builder) {
         HandlerInfo.Builder b = new HandlerInfo.Builder(handlerName, modName, modId);
         builder.accept(b);
         HandlerInfo info = b.build();
-        if (GuiRecipeTab.handlerMap.put(info.getHandlerName(), info) != null) {
-            NEIClientConfig.logger.info("Replaced handler info for {}", info.getHandlerName());
-        } else {
-            NEIClientConfig.logger.info("Added handler info for {}", info.getHandlerName());
-        }
+        registerHandlerInfo(info);
     }
 
     public void registerHandlerInfo(Class<? extends IRecipeHandler> handlerClazz, String modName, String modId,
